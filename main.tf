261ca36e507810f119bef405612662bed8f5d4c5
PawanSenapati <51953796+PawanSenapati@users.noreply.github.com>
2023-03-18 06:13:44 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "google" {
+  project = "gcp-devops-376307"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_cloud_run_service" "app_service" {
+  name     = "devops-interview"
+  location = "us-central1"
+  
+  template {
+    spec {
+      containers {
+        image = "gcr.io/gcp-devops-376307/devops-init:v2.0"
+      }
+    }
+  }
+}
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.app_service.location
+  project = "gcp-devops-376307"
+  service     = google_cloud_run_service.app_service.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
