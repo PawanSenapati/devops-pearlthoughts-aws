a1e0e7ae43f3aad50bf55d19cbbd6ec0719edd50
PawanSenapati <51953796+PawanSenapati@users.noreply.github.com>
2023-03-19 04:04:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = ${{secrets.AWS_ACCESS_KEY}}
-  secret_key = ${{secrets.AWS_SECRET_KEY}}
+  access_key = ${secrets.AWS_ACCESS_KEY}
+  secret_key = ${secrets.AWS_SECRET_KEY}
 }
 
 variable "image_tag" {
