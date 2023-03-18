415c7775511f8ee01316cae4f97f9e3b63adeaea
PawanSenapati <51953796+PawanSenapati@users.noreply.github.com>
2023-03-19 04:44:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -122,3 +122,29 @@ resource "aws_ecs_service" "ecs_service" {
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = "my-ecs-cluster"
 }
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "load-balancer-dev" #load balancer name
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_subnet.ecs_subnet.id}"
+  ]
+  # security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
