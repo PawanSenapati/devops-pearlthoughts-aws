4553d0935e34957f6f2c3ad8d3e69b0bb038f9f2
PawanSenapati <51953796+PawanSenapati@users.noreply.github.com>
2023-03-19 06:54:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -147,7 +147,7 @@ resource "aws_security_group" "load_balancer_security_group" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "http"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
   }
 
